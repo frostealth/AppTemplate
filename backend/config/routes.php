7b67d8f87e4d6bea7a38227fb7a12824a187947e
frostealth <frostealth@yandex.ru>
2013-05-27 21:09:09 +0700

    fix routes

--- backend/config/routes.php
+++ backend/config/routes.php
@@ -2,4 +2,10 @@
 
 return array(
     '' => 'dashboard/index',
+
+    // Default
+    '<controller:\w+>' => '<controller>/index',
+    '<controller:\w+>/<id:\d+>' => '<controller>/view',
+    '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
 );
